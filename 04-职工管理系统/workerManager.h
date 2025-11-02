a4833a6f3f3018f70c294a970f423c2f873278ea
fyc <1805800367@qq.com>
2025-11-02 22:33:23 +0800

    职工管理系统初始化

---
+++ 04-职工管理系统/workerManager.h
@@ -0,0 +1,16 @@
+#pragma once // 防止头文件重复包含
+#include <iostream>
+using namespace std;
+
+class WorkerManager {
+public:
+	WorkerManager();
+
+	// 展示菜单
+	void Show_Menu();
+
+	// 退出系统
+	void ExitSystem();
+
+	~WorkerManager();
+};
---
+++ 04-职工管理系统/职工管理系统.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+#include "workerManager.h"
+
+#include "worker.h"
+#include "employee.h"
+
+#define x first
+#define y second
+#define null NULL
+#define endl "\n"
+#define rep(i, m, n) for (int i = (m); i <= (n); ++i)
+#define rrep(i, m, n) for (int i = (m); i >= (n); --i)
+#define debug(n, m) cout << #n << " = " << (m) << endl
+#define cout(n) cout << n << endl
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef long long ll;
+
+/*
+
+*/
+
+int main(int argc, char* argv[]) {
+	WorkerManager wm;
+
+	int choice = 0;
+	while (true) {
+		wm.Show_Menu();
+		cout("请输入您的选择:");
+		cin >> choice;
+
+		switch (choice) {
+		case 0: // 退出系统
+			wm.ExitSystem();
+			break;
+		case 1: // 增加职工
+			break;
+		case 2: // 显示职工
+			break;
+		case 3: // 删除职工
+			break;
+		case 4: // 修改职工
+			break;
+		case 5: // 查号职工
+			break;
+		case 6: // 排序职工
+			break;
+		case 7: // 清空职工
+			break;
+		default: 
+			system("cls"); // 清屏
+			break;
+		}
+	}
+
+	// system("pause");
+	return EXIT_SUCCESS;
+}
