bb61c3ad36cef6f00038bdd9670a28134847b668
fyc <1805800367@qq.com>
2025-11-25 22:39:08 +0800

    2025/11/25 22:38

--- 06-CplusplusSTL/002-string容器.cpp
+++ 06-CplusplusSTL/002-string容器.cpp
@@ -35,6 +35,17 @@ typedef long long ll;
 		- string& append(const char *s, int n); //把字符串s的前n个字符连接到当前字符串结尾
 		- string& append(const string &s); //同operator+=(const string& str)
 		- string& append(const string &s, int pos, int n); //字符串s中从pos开始的n个字符连接到字符串结尾
+	4. 查找与替换
+		- int find(const string& str, int pos = 0) const;  //查找str第一次出现位置,从pos开始查找
+		- int find(const char* s, int pos = 0) const;   //查找s第一次出现位置,从pos开始查找
+		- int find(const char* s, int pos, int n) const; //从pos位置查找s的前n个字符第一次位置
+		- int find(const char c, int pos = 0) const; //查找字符c第一次出现位置
+		- int rfind(const string& str, int pos = npos) const;   //查找str最后一次位置,从pos开始查找
+		- int rfind(const char* s, int pos = npos) const;  //查找s最后一次出现位置,从pos开始查找
+		- int rfind(const char* s, int pos, int n) const;  //从pos查找s的前n个字符最后一次位置
+		- int rfind(const char c, int pos = 0) const;  //查找字符c最后一次出现位置
+		- string& replace(int pos, int n, const string& str);  //替换从pos开始n个字符为字符串str
+		- string& replace(int pos, int n,const char* s); //替换从pos开始的n个字符为字符串s
 */
 
 // 构造函数
@@ -82,10 +93,33 @@ void test02_03() {
 	cout(s3); // I love game LOL CF!!!
 }
 
+// 查找
+void test02_04() {
+	string str1 = "abcdefgdefg";
+	cout << str1.find("de") << endl; // 3, 下标
+	cout << (int) str1.find("ac") << endl; // -1, 未找到
+	cout << str1.rfind("de") << endl; // 7, 下标
+}
+
+// 替换
+void test02_05() {
+	string str1 = "abcdefg";
+	cout << str1.replace(1, 3, "1111") << endl; // a1111efg
+	cout << str1.replace(1, 3, str1) << endl; // aa1111efg1efg
+	cout << str1 << endl; // aa1111efg1efg
+}
+
+void test02_06() {
+
+}
+
+
 int main(int argc, char* argv[]) {
-	test02_01();
-	test02_02();
-	test02_03();
+	//test02_01();
+	//test02_02();
+	//test02_03();
+	test02_04();
+	test02_05();
 	// system("pause");
 	return EXIT_SUCCESS;
 }
