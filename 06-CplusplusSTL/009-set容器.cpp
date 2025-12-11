b7e1a042a4d0629a4ddef59acebe916070d178d7
fyc <1805800367@qq.com>
2025-12-11 21:34:50 +0800

    2025/12/11 21:34

--- 06-CplusplusSTL/009-set容器.cpp
+++ 06-CplusplusSTL/009-set容器.cpp
@@ -72,11 +72,52 @@ void test09_04() {
 	cout << "元素1的个数: " << s.count(1) << endl; // 1
 }
 
+// set与multiset的区别
+void test09_05() {
+	set<int> s;
+	pair<set<int>::iterator, bool> res = s.insert(10);
+	cout << "第一次插入" << (res.y ? "成功!" : "失败!") << endl;
+	res = s.insert(10);
+	cout << "第二次插入" << (res.y ? "成功!" : "失败!") << endl;
+
+	multiset<int> ms{ 10, 10, 10 };
+	for (multiset<int>::iterator it = ms.begin(); it != ms.end(); ++it)
+		cout << *it << " ";
+	cout << endl;
+}
+
+// 创建pair
+void test09_06() {
+	pair<string, int> p1("tom", 23);
+	pair<string, int> p2 = make_pair("jerry", 28);
+	cout << "姓名: " << p1.first << ", 年龄: " << p1.second << endl;
+	cout << "姓名: " << p2.first << ", 年龄: " << p2.second << endl;
+}
+
+// set容器排序 - 仿函数
+class MyCompare {
+public:
+	bool operator()(int a, int b) const {
+		return a > b;
+	}
+};
+void test09_07() {
+	set<int> s1{ 10, 30, 60, 20, 50, 40 }; printSet(s1);
+
+	set<int, MyCompare> s2;
+	for (auto it = s1.begin(); it != s1.end(); ++it) s2.insert(*it);
+	for (auto it = s2.begin(); it != s2.end(); ++it) cout << *it << " ";
+	cout << endl;
+}
+
 int main(int argc, char* argv[]) {
 	//test09_01();
 	//test09_02();
 	//test09_03();
-	test09_04();
+	//test09_04();
+	//test09_05();
+	//test09_06();
+	test09_07();
 	// system("pause");
 	return EXIT_SUCCESS;
 }
