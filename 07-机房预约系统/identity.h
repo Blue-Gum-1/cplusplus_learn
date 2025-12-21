a918eca9acd7273dd25338c01ef65c1e08a3f495
fyc <1805800367@qq.com>
2025-12-21 21:34:33 +0800

    2025/12/21 21:34

---
+++ 07-机房预约系统/identity.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <bits/stdc++.h>
+#define x first
+#define y second
+#define null NULL
+#define endl "\n"
+#define rep(i, m, n) for (int i = (m); i <= (n); ++i)
+#define rrep(i, m, n) for (int i = (m); i >= (n); --i)
+#define repit(it, m) for (auto it = (m.begin()); it != (m.end()); ++it)
+#define debug(n, m) cout << #n << " = " << (m) << endl
+#define cout(n) cout << n << endl
+using namespace std;
+
+// 身份抽象类
+class Identity {
+public:
+	// 操作菜单
+	virtual void operMenu() = 0;
+
+	string m_Name; // 用户名
+	string m_Pwd; // 密码
+};
---
+++ 07-机房预约系统/机房预约系统.cpp
@@ -0,0 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "globalFile.h"
+#include "student.h"
+#include "teacher.h"
+#include "manager.h"
+
+typedef pair<int, int> PII;
+typedef long long ll;
+
+/*
+
+*/
+
+// fileName: 操作的文件名, type: 登录的身份(1 学生, 2 老师, 3 管理员)
+void LoginIn(string fileName, int type) {
+    Identity* person = null;
+    ifstream ifs;
+    ifs.open(fileName, ios::in);
+
+    if (!ifs.is_open()) {
+        cout << fileName << " 不存在" << endl;
+        ifs.close();
+        return;
+    }
+
+    int id, fId;
+    string name, pwd, fName, fPwd;
+    if (type == 1) { // 学生
+        cout << "请输入你的学号" << endl;
+        cin >> id;
+    }
+    else if (type == 2) { // 老师
+        cout << "请输入你的职工号" << endl;
+        cin >> id;
+    }
+
+    cout << "请输入用户名：" << endl;
+    cin >> name;
+
+    cout << "请输入密码： " << endl;
+    cin >> pwd;
+    if (type == 1) {
+        //学生登录验证
+        while (ifs >> fId && ifs >> fName && ifs >> fPwd) {
+            if (id == fId && name == fName && pwd == fPwd) {
+                cout << "学生验证登录成功!" << endl;
+                system("pause");
+                system("cls");
+                person = new Student(id, name, pwd);
+                return;
+            }
+        }
+    } else if (type == 2) {
+        //教师登录验证
+        while (ifs >> fId && ifs >> fName && ifs >> fPwd) {
+            if (id == fId && name == fName && pwd == fPwd) {
+                cout << "教师验证登录成功!" << endl;
+                system("pause");
+                system("cls");
+                person = new Teacher(id, name, pwd);
+                return;
+            }
+        }
+    } else if (type == 3) {
+        //管理员登录验证
+        while (ifs >> fName && ifs >> fPwd) {
+            if (name == fName && pwd == fPwd) {
+                cout << "验证登录成功!" << endl;
+                //登录成功后，按任意键进入管理员界面
+                system("pause");
+                system("cls");
+                //创建管理员对象
+                person = new Manager(name, pwd);
+                return;
+            }
+        }
+    }
+
+    cout << "验证登录失败!" << endl;
+    system("pause");
+    system("cls");
+    return;
+}
+
+void test01() {
+    int select = 0;
+    while (true) {
+        cout << "======================  欢迎来到传智播客机房预约系统  =====================" << endl;
+        cout << endl << "请输入您的身份" << endl;
+        cout << "\t\t -------------------------------\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          1.学生代表           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          2.老    师           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          3.管 理 员           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t|          0.退    出           |\n";
+        cout << "\t\t|                               |\n";
+        cout << "\t\t -------------------------------\n";
+        cout << "输入您的选择: ";
+
+        cin >> select; //接受用户选择
+        switch (select) {
+        case 1:  //学生身份
+            LoginIn(STUDENT_FILE, 1);
+            break;
+        case 2:  //老师身份
+            LoginIn(TEACHER_FILE, 2);
+            break;
+        case 3:  //管理员身份
+            LoginIn(ADMIN_FILE, 3);
+            break;
+        case 0:  //退出系统
+            cout << "欢迎下一次使用" << endl;
+            system("pause");
+            return;
+            break;
+        default:
+            cout << "输入有误，请重新选择！" << endl;
+            system("pause");
+            system("cls");
+            break;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+	test01();
+	// system("pause");
+	return EXIT_SUCCESS;
+}
